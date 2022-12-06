5050f5a6a477c13b9adeae076b68f9dd6d2cf21b
manhdev <manhlam.mobe@gmail.com>
2022-12-06 20:03:22 +0700

    vanmanhupdate2233

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<div className='App'>
-			<h1>Tao là mạnh đây</h1>
-			<h1>Tao là Tri abc đây</h1>
-			<h1>Tao là Nam abc đây</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Tao là mạnh đây</h1>
+      <h1>Tao là Tri abc đây</h1>
+      <h1>Tao là Nam2323 abc đây</h1>
+    </div>
+  );
 }
 
 export default App;
