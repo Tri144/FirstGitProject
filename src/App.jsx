a7c0f1f60c93de110bca8aa6f9794ae9b5a6a194
Tri <tringd140400@gmail.com>
2022-12-06 20:11:58 +0700

    tri

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,14 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-<<<<<<< HEAD
   return (
     <div className="App">
       <h1>Tao là mạnh đây</h1>
       <h1>Tao là Tri abc đây</h1>
-      <h1>Tao là Nam2323 abc đây</h1>
+      <h1>Tao là Nam abc đây</h1>
+      <h1>Em là Sieu Troi abc đây</h1>
     </div>
   );
-=======
-	return (
-		<div className='App'>
-			<h1>Tao là mạnh đây</h1>
-			<h1>Tao là Tri abc đây</h1>
-			<h1>Tao là Nam abc đây</h1>
-			<h1>Em là Sieu Troi abc đây</h1>
-		</div>
-	);
->>>>>>> 0a933ab1276841df02547d0a65693fe2492c9d80
 }
 
 export default App;
